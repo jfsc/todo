5ad6c79c415a08a1a214da3f6c7f60aebcb847d5
LEONARDO DE AGUIAR SILVA RAMALHO <leonardo.ramalho@maisunifacisa.com.br>
2025-12-11 17:23:01 -0300

    test: add testing for updating, deleting and listing

--- src/test/java/com/example/todo/repository/InMemoryTodoRepositoryTest.java
+++ src/test/java/com/example/todo/repository/InMemoryTodoRepositoryTest.java
@@ -1,8 +1,11 @@
 package com.example.todo.repository;
 
 import com.example.todo.domain.Todo;
+import com.example.todo.usecase.TodoService;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,26 +24,51 @@ class InMemoryTodoRepositoryTest {
 
     @Test
     void shouldCreateTodo() {
-        Todo todo = new Todo(UUID.randomUUID(), "Test", "teste de criacao", false);
+        Todo todo = new Todo(UUID.randomUUID(), "Create Test", "Teste de Criacao", false);
         repository.save(todo);
 
         Optional<Todo> found = repository.findById(todo.getId());
         assertTrue(found.isPresent());
-        assertEquals("Test", found.get().getTitle());
+        assertEquals("Create Test", found.get().getTitle());
     }
 
 
-//    @Test
-//    void shouldListTodos() {
-//
-//
-//    @Test
-//    void shouldUpdateTodo() {
-//
-//    }
-//
-//    @Test
-//    void shouldDeleteTodo() {
-//
-//    }
+      @Test
+      void shouldListTodos() {
+    	Todo todo = new Todo(UUID.randomUUID(), "List Test", "Teste de Lista", false);
+        repository.save(todo);
+        
+        List<Todo> foundTodos = repository.findAll();
+        assertTrue(!foundTodos.isEmpty());
+      }
+
+
+      @Test
+      void shouldUpdateTodo() {
+    	      	  
+        Todo todo = new Todo(UUID.randomUUID(), "Update Test", "Teste de Update", false);
+        repository.save(todo);
+        
+        Todo updatedTodo = repository.findById(todo.getId()).orElseThrow(() -> new RuntimeException("Não foi possível encontrar a tarefa."));
+        
+        updatedTodo.setTitle("Update Test (2)");
+        updatedTodo.setDescription("Teste de Update Modificado");
+        updatedTodo.setDone(true);
+        repository.save(todo);
+        
+        assertNotEquals("Update Test", repository.findById(todo.getId()).get().getTitle());
+        assertEquals(todo.getId(), updatedTodo.getId());
+      }
+
+      @Test
+      void shouldDeleteTodo() {
+
+        Todo todo = new Todo(UUID.randomUUID(), "Update Test", "Teste de Update", false);
+        repository.save(todo);
+        
+        Todo foundTodo = repository.findById(todo.getId()).orElseThrow(() -> new RuntimeException("Não foi possível encontrar a tarefa."));
+        repository.deleteById(foundTodo.getId());
+        
+        assertTrue(repository.findById(todo.getId()).isEmpty());	  
+    }
 }
