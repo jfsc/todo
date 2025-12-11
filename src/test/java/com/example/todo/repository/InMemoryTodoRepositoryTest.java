c42054e02542b7e185f2a773d91c56c97bce5683
brunaxmello <bruna.mello@maisunifacisa.com.br>
2025-12-11 18:10:48 -0300

    adding test coverage

--- src/test/java/com/example/todo/repository/InMemoryTodoRepositoryTest.java
+++ src/test/java/com/example/todo/repository/InMemoryTodoRepositoryTest.java
@@ -29,18 +29,43 @@ class InMemoryTodoRepositoryTest {
         assertEquals("Test", found.get().getTitle());
     }
 
+    @Test
+    void shouldListTodos() {
+        Todo todo1 = new Todo(UUID.randomUUID(), "Teste 1", "teste de criação 1", false);
+        Todo todo2 = new Todo(UUID.randomUUID(), "Teste 2", "teste de criação 2", true);
 
-//    @Test
-//    void shouldListTodos() {
-//
-//
-//    @Test
-//    void shouldUpdateTodo() {
-//
-//    }
-//
-//    @Test
-//    void shouldDeleteTodo() {
-//
-//    }
+        repository.save(todo1);
+        repository.save(todo2);
+
+        List<Todo> todos = repository.findAll();
+
+    }
+
+    @Test
+    void shouldUpdateTodo() {
+        UUID id = UUID.randomUUID();
+        Todo existingTodo = new Todo(id, "Test Title", "Test Description", false);
+        repository.save(existingTodo);
+
+        Todo updated = new Todo(id, "Updated Title", "Updated Description", true);
+        repository.save(updated);
+
+        Optional<Todo> result = repository.findById(id);
+        assertTrue(result.isPresent());
+        assertEquals("Updated Title", result.get().getTitle());
+    }
+
+    @Test
+    void shouldDeleteTodo() {
+        UUID id = UUID.randomUUID();
+        Todo existingTodo = new Todo(id, "To Delete", "Will be deleted", false);
+        repository.save(existingTodo);
+
+        assertTrue(repository.findById(id).isPresent());
+        repository.deleteById(id);
+
+        Optional<Todo> result = repository.findById(id);
+        assertFalse(result.isPresent());
+    }
+
 }
