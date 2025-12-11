9a5d8f76a9e03ad325c5e5b3f1acbc79fa7fa209
Kamila Queiroz <kamilapaqueiroz@gmail.com>
2025-12-11 16:58:27 -0300

    tests implementation and full test coverage

--- src/main/java/com/example/todo/repository/InMemoryTodoRepository.java
+++ src/main/java/com/example/todo/repository/InMemoryTodoRepository.java
@@ -24,4 +24,7 @@ public class InMemoryTodoRepository {
     public Optional<Todo> findById(UUID id) { return Optional.ofNullable(store.get(id)); }
     public List<Todo> findAll() { return store.values().stream().collect(Collectors.toList()); }
     public void deleteById(UUID id) { store.remove(id); }
+    public void update(UUID id, Todo todo)  {
+        store.put(id, todo);
+    }
 }
--- src/test/java/com/example/todo/repository/InMemoryTodoRepositoryTest.java
+++ src/test/java/com/example/todo/repository/InMemoryTodoRepositoryTest.java
@@ -30,17 +30,48 @@ class InMemoryTodoRepositoryTest {
     }
 
 
-//    @Test
-//    void shouldListTodos() {
-//
-//
-//    @Test
-//    void shouldUpdateTodo() {
-//
-//    }
-//
-//    @Test
-//    void shouldDeleteTodo() {
-//
-//    }
+    @Test
+    void shouldListTodos() {
+        Todo todo1 = new Todo(UUID.randomUUID(), "A", "descricao A", false);
+        Todo todo2 = new Todo(UUID.randomUUID(), "B", "descricao B", true);
+
+        repository.save(todo1);
+        repository.save(todo2);
+
+        List<Todo> todos = repository.findAll();
+
+        assertEquals(2, todos.size());
+        assertTrue(todos.contains(todo1));
+        assertTrue(todos.contains(todo2));
+    }
+
+    @Test
+    void shouldUpdateTodo() {
+        UUID id = UUID.randomUUID();
+        Todo original = new Todo(id, "Original", "descricao teste", false);
+        repository.save(original);
+
+        Todo updated = new Todo(id, "Atualizado", "nova desceicao teste", true);
+        repository.update(id, updated);
+
+        Optional<Todo> found = repository.findById(id);
+
+        assertTrue(found.isPresent());
+        assertEquals("Atualizado", found.get().getTitle());
+        assertEquals("nova desceicao teste", found.get().getDescription());
+        assertTrue(found.get().isDone());
+    }
+
+    @Test
+    void shouldDeleteTodo() {
+        UUID id = UUID.randomUUID();
+        Todo todo = new Todo(id, "Deletar", "deletar", false);
+
+        repository.save(todo);
+        repository.deleteById(id);
+
+        Optional<Todo> found = repository.findById(id);
+
+        assertTrue(found.isEmpty(), "O todo deveria ter sido deletado");
+    }
 }
