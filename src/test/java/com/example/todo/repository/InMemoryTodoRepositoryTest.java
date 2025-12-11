5bd777aa9d22fd0d68c2ceeba211225df9910de0
Gabriel Freitas <62771225+Mordecai456Br@users.noreply.github.com>
2025-12-11 17:31:44 -0300

    feature_testing_with_JUNIT

--- src/test/java/com/example/todo/repository/InMemoryTodoRepositoryTest.java
+++ src/test/java/com/example/todo/repository/InMemoryTodoRepositoryTest.java
@@ -33,17 +33,55 @@ class InMemoryTodoRepositoryTest {
     @Test
     void shouldListTodos() {
 
+        Todo todo1 = new Todo(UUID.randomUUID(),"Learn JUnit5", "You should learn JUnit", false);
+        Todo todo2 = new Todo(UUID.randomUUID(),"Finish logic in Industrialist", "This should be make in Figma", false);
+        Todo todo3 = new Todo(UUID.randomUUID(),"Learn to use AI", "Learn how to learn fast with AI", true);
+
+        repository.save(todo1);
+        repository.save(todo2);
+        repository.save(todo3);
+
+        List<Todo> todos = repository.findAll();
+
+        assertEquals(3,todos.size());
+        assertTrue(todos.contains(todo1));
+        assertTrue(todos.contains(todo2));
+        assertTrue(todos.contains(todo3));
 
     }
 
+    @Test
+    void shouldUpdateTodo() {
+        Todo todo1 = new Todo(UUID.randomUUID(),"Learn JUnit5", "You should learn JUnit", false);
+        repository.save(todo1);
 
-//    @Test
-//    void shouldUpdateTodo() {
-//
-//    }
-//
-//    @Test
-//    void shouldDeleteTodo() {
-//
-//    }
+        todo1.setTitle("Finish logic in Industrialist");
+        todo1.setDescription("Updated description");
+        todo1.setDone(true);
+
+        repository.save(todo1);
+
+        Optional<Todo> updated = repository.findById(todo1.getId());
+
+        assertTrue(updated.isPresent());
+        assertEquals("Finish logic in Industrialist", updated.get().getTitle());
+        assertEquals("Updated description", updated.get().getDescription());
+        assertTrue(updated.get().isDone());
+
+  }
+
+    @Test
+    void shouldDeleteTodo() {
+
+        Todo todo = new Todo(UUID.randomUUID(), "Finish logic in Industrialist", "This should be make in Figma", false);
+        repository.save(todo);
+        Optional<Todo> found = repository.findById(todo.getId());
+        assertTrue(found.isPresent());
+
+
+        repository.deleteById(todo.getId());
+        Optional<Todo> deleted = repository.findById(todo.getId());
+
+        assertFalse(deleted.isPresent());
+    }
 }
