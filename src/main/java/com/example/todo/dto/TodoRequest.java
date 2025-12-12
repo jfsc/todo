199a11fe4059017fe8d1ef270c24b03f6cd11281
Mordecai456Br <gabrielseixas1@gmail.com>
2025-12-12 01:09:22 -0300

    feature - Request no longer has gets

--- src/main/java/com/example/todo/dto/TodoRequest.java
+++ src/main/java/com/example/todo/dto/TodoRequest.java
@@ -10,11 +10,9 @@ public class TodoRequest {
     private String description;
     private boolean done;
 
-    public String getTitle() { return title; }
+
     public void setTitle(String title) { this.title = title; }
-    public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
-    public boolean isDone() { return done; }
     public void setDone(boolean done) { this.done = done; }
 
     public Todo toDomain() {
