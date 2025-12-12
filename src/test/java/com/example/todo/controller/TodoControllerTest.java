466a0b9db7231c47e06e3fdfe688bee6e81f3d28
Mordecai456Br <gabrielseixas1@gmail.com>
2025-12-12 04:05:36 -0300

    feature TodoController autogenerated with intelij (NEED REVIEW TO TEST IT RIGHT)

---
+++ src/test/java/com/example/todo/controller/TodoControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.todo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoControllerTest {
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
