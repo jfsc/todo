591ac399d7d0fde03f4c38ed10201077d5fe535b
Mordecai456Br <gabrielseixas1@gmail.com>
2025-12-12 13:33:51 -0300

    feature TodoController now has setup and test for create

--- src/test/java/com/example/todo/controller/TodoControllerTest.java
+++ src/test/java/com/example/todo/controller/TodoControllerTest.java
@@ -1,17 +1,51 @@
 package com.example.todo.controller;
 
+import com.example.todo.domain.Todo;
+import com.example.todo.dto.TodoRequest;
+
+import com.example.todo.dto.TodoResponse;
+import com.example.todo.repository.InMemoryTodoRepository;
+import com.example.todo.usecase.TodoService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TodoControllerTest {
 
+    private InMemoryTodoRepository repository;
+    private TodoService service;
+    private TodoController controller;
+
+
+    @BeforeEach
+    void setup() {
+        repository = new InMemoryTodoRepository();
+        service = new TodoService(repository);
+        controller = new TodoController(service);
+    }
+
     @Test
     void list() {
+      
     }
 
     @Test
     void create() {
+        TodoRequest req = new TodoRequest();
+        req.setTitle("test");
+        req.setDescription("test");
+        req.setDone(false);
+
+        ResponseEntity<TodoResponse> created = controller.create(req);
+
+        Optional<Todo> found = repository.findById(created.getBody().getId());
+        assertTrue(found.isPresent());
+
     }
 
     @Test
