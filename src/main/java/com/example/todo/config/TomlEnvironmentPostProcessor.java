28fd3c87976d88a81989e54697917b62d79eaf63
Samuel-Marcos-Holanda <samuel.marcos.smh@gmail.com>
2025-12-12 16:19:00 -0300

    changes: custom loadResource() for enabling creation of child classes in unit testing

--- src/main/java/com/example/todo/config/TomlEnvironmentPostProcessor.java
+++ src/main/java/com/example/todo/config/TomlEnvironmentPostProcessor.java
@@ -16,10 +16,14 @@ public class TomlEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     private static final String NAME = "tomlPropertySource";
 
+    protected Resource loadResource() {
+        return new ClassPathResource("confd_spring/app_config.toml");
+    }
+
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         try {
-            Resource res = new ClassPathResource("confd_spring/app_config.toml");
+            Resource res = loadResource();
             if (!res.exists()) {
                 System.out.println("TOML config not found: confd_spring/app_config.toml");
                 return;
@@ -38,4 +42,6 @@ public class TomlEnvironmentPostProcessor implements EnvironmentPostProcessor {
             System.err.println("Failed to load TOML config: " + e.getMessage());
         }
     }
+
+    public static String getName() { return NAME; }
 }
